9590b9ebe1e8ef2d6858417109ed442262fa03ec
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-12-18 01:12:28 +0200

    Fixed bug for user registration & permit non-devise params for user

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname, :gender, :hometown, :about_me, :birthdate])
-      devise_parameter_sanitizer.permit(:edit, keys: [:first_name, :last_name, :nickname, :gender, :hometown, :about_me, :birthdate])
+      params = [ :first_name, :last_name, :nickname, :gender,
+        :hometown, :about_me, :birthdate, :marital_status ]
+
+      devise_parameter_sanitizer.permit(:sign_up, keys: params)
+      devise_parameter_sanitizer.permit(:edit, keys: params)
     end
 
 end
