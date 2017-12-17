3a0387500a977b873ff2be8c53a66bd15911a28a
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-12-17 23:29:34 +0200

    Added attr to user model.

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname, :gender, :hometown, :about_me, :birthdate])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :gender, :hometown, :about_me, :birthdate])
+    end
+
 end
