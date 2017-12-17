5eb2e87eee625d47dffbb80e26a157f17bbc9ddb
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-12-17 23:34:56 +0200

    Installed rspec gem for testing.

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname, :gender, :hometown, :about_me, :birthdate])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :gender, :hometown, :about_me, :birthdate])
+      devise_parameter_sanitizer.permit(:edit, keys: [:first_name, :last_name, :nickname, :gender, :hometown, :about_me, :birthdate])
     end
 
 end
