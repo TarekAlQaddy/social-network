818b38d1df28454b8f7a043443e3189fda3e973e
tarekalqaddy <tarek.alqaddy@gmail.com>
2017-12-18 18:55:22 +0200

    Add login main view with forms & validation.

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Main from '@/components/Main'
+import Login from '@/components/Login'
 import FriendsView from '@/components/friends/FriendsView'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
     {
       path: '/',
       name: 'main',
