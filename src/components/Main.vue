168c9d7b8c83770a5ead2147721991095c39aafe
tarekalqaddy <tarek.alqaddy@gmail.com>
2017-12-18 02:01:40 +0200

    Add sidebar & home layouts.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,48 @@
 <template>
   <div>
-    <!-- <div class="ui visible vertical large inverted menu sidebar">
-      <a class="item">
-        Item 1
-      </a>
-      <a class="item">
-        Item 2
-      </a>
-      <a class="item">
-        Item 3
-      </a>
-    </div> -->
-    <div class="pusher">
-      Your site's actual content
+    <div id="main-sidebar" class="ui visible labeled icon vertical large inverted menu sidebar">
+      <div id="user-image-container" style="background-image: url('static/js.svg')">
+      </div>
+      <router-link :to="{ name: 'home' }">
+        <a class="item">
+          <i class="home icon"></i>
+          Home
+        </a>
+      </router-link>
+      <router-link :to="{ name: 'home' }">
+        <a class="item">
+          <i class="user icon"></i>
+          Profile
+        </a>
+      </router-link>
+      <router-link :to="{ name: 'home' }">
+        <a class="item">
+          <i class="users icon"></i>
+          Friends
+        </a>
+      </router-link>
+      <router-link :to="{ name: 'home' }">
+        <a class="item">
+          <i class="search icon"></i>
+          Search
+        </a>
+      </router-link>
+      <router-link :to="{ name: 'home' }">
+        <a class="item">
+          <i class="sign out icon"></i>
+          Logout
+        </a>
+      </router-link>
+    </div>
+    <!--<div style="margin-left: 18rem" class="ui top fixed huge inverted menu">-->
+      <!--<div class="item">-->
+        <!--Home-->
+      <!--</div>-->
+    <!--</div>-->
+    <div style="margin-left: 18rem">
+      <div class="ui container">
+        <router-view/>
+      </div>
     </div>
   </div>
 </template>
@@ -21,13 +51,18 @@
   export default {
     data () {
       return {}
-    },
-    components: {
-
     }
   }
 </script>
 
 <style scoped>
-
+  #user-image-container {
+    margin: 35% auto 50px auto;
+    width: 15rem;
+    height: 15rem;
+    border-radius: 50%;
+    display: block;
+    background: no-repeat center center;
+    background-size: cover;
+  }
 </style>
