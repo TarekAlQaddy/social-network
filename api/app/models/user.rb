761de5bc48a83affa2be75d4102fc4aa3bd4b3bc
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-12-19 23:30:41 +0200

    Added profile_picture to user.

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ActiveRecord::Base
+  # User Posts & Phone Number
   has_many :posts, dependent: :destroy
   has_many :phones, dependent: :destroy
 
-  # user friendship
+  # User friendship
   has_many :friendships
   has_many :friends, through: :friendships, source: :friend
+
+  # User profile picture
+  has_attached_file :profile_picture,
+                    styles: { medium: "300x300>", thumb: "100x100>" },
+                    default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :profile_picture, content_type: /\Aimage\/.*\z/
 
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218133302) do
+ActiveRecord::Schema.define(version: 20171219212835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,10 @@ ActiveRecord::Schema.define(version: 20171218133302) do
     t.date "birthdate"
     t.string "marital_status"
     t.text "about_me"
+    t.string "profile_picture_file_name"
+    t.string "profile_picture_content_type"
+    t.integer "profile_picture_file_size"
+    t.datetime "profile_picture_updated_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
