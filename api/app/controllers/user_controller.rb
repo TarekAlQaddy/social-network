574e3d887c941317ea58792668ad813530876b76
Mahmoud Magdy <memo90.mcsofts@yahoo.com>
2017-12-19 20:35:54 +0200

    Fetch current user fix

--- api/app/controllers/user_controller.rb
+++ api/app/controllers/user_controller.rb
@@ -1,4 +1,7 @@
 class UserController < ApplicationController
+
+  before_action :authenticate_user!, only: [:fetch_current_user]
+
   def show
     if params.key?(:id)
       render json: User.select(:id, :nickname, :image, :first_name,
@@ -8,4 +11,9 @@ class UserController < ApplicationController
       render status: :unauthroized
     end
   end
+
+  def fetch_current_user
+    render json: current_user, status: :ok
+  end
+
 end
