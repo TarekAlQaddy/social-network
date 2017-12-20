b7784a5c1dcc42d09b6d9fc4edf918feefb7c9c2
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-12-20 03:06:08 +0200

    Added posts controller.

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :friendships, only: [:index, :create, :destroy]
+
+  resources :posts, only: [:index, :create, :destroy]
+
   mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
